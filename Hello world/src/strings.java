b80bcac7fb85dca11c7ef5afcef7dd6fc4886591
maneeshreddy <karrisaimaneeshkumarreddy@gmail.com>
2022-07-14 10:02:33 +0530

    adding strings

---
+++ Hello world/src/strings.java
@@ -0,0 +1,14 @@
+
+public class strings {
+
+	public static void main(String[] args) {
+		// string literal
+				String name = "Maneesh reddy";
+				
+				System.out.println("Name " + name);
+				System.out.println("Name length " + name.length());
+
+	
+	}
+
+}
